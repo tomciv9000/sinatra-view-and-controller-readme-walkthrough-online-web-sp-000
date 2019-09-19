fee1de7e93133031efc50e681224b540ead69ea2
Tom White <eugene.white@gmail.com>
2019-09-19 05:39:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
     get '/friends' do
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
  
-  erb :friends
+    erb :friends
   end
   
 end
